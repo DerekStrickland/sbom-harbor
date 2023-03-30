01ef7b2dd4074d3d3af92c64c6c46f417789a5ea
qtpeters <quinn.t.peters@gmail.com>
2023-03-30 08:59:03 -0400

    feat(gh_provider): Added first test

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -40,7 +40,6 @@ impl Opts for PilotOpts {
     }
 }
 
-// TODO Should this be a trait?
 pub struct PilotCommand {}
 
 impl PilotCommand {
@@ -98,4 +97,16 @@ fn get_env_var(variable_name: &str) -> Option<String> {
         Ok(v) => Some(v),
         Err(e) => None,
     };
+}
+
+#[tokio::test]
+async fn test_get_env_var() {
+    let var_name: &str = "TEST_ENV_VAR";
+    let var_value: &str = "testvalue";
+    env::set_var(var_name, var_value);
+
+    match get_env_var(var_name) {
+        Some(value) => assert_eq!(var_value, value),
+        None => panic!("No test value...?")
+    }
 }
